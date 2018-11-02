3eecb804da7569b7b65cc4c368d459c81338481a
Chris Hellmuth <chellmuth@gmail.com>
2018-11-02 11:47:39 -0700

    wrapper 1 -> n wip

--- matlab_wrapper.py
+++ matlab_wrapper.py
@@ -1,12 +1,13 @@
 import subprocess
 import struct
 
-def _write_randoms(randoms, out_path):
+def _write_randoms(randoms_matrix, out_path):
     out_file = open(out_path, "wb")
 
     bin_floats = []
 
-    bin_floats.append(struct.pack("f" * len(randoms), *randoms))
+    for randoms in randoms_matrix:
+        bin_floats.append(struct.pack("f" * len(randoms), *randoms))
 
     out_file.write(struct.pack("I", len(bin_floats)))
     for bin_float in bin_floats:
@@ -14,16 +15,33 @@ def _write_randoms(randoms, out_path):
 
     out_file.close()
 
-def read_luminance(in_path):
+def read_luminances(in_path):
     f = open(in_path, "rb")
 
-    luminance, = struct.unpack("f", f.read(4))
-    return luminance
+    luminances = []
 
-def f(args):
-    _write_randoms(args, "randoms.bin")
-    subprocess.call(
-        ["mitsuba", "cjh.xml"],
+    byte = f.read(4)
+    while byte:
+        luminance, = struct.unpack("f", byte)
+        byte = f.read(4)
+
+    return luminances
+
+def f_1(randoms_1):
+    _write_randoms([randoms_1], "randoms.bin")
+    return _f()[0]
+
+def f_n(randoms_n):
+    randoms_n = list(randoms_n)
+    _write_randoms(randoms_n, "randoms.bin")
+    return _f()
+
+def _f():
+    return_code = subprocess.call(
+        ["mitsuba", "staircase/cjh-scene.xml"],
         env={"PATH": "./dist", "LD_LIBRARY_PATH": "./dist"}
     )
-    return read_luminance("luminance.bin")
+    if return_code != 0:
+        raise BaseException("Failed call to Mitsuba")
+
+    return read_luminances("luminance.bin")
