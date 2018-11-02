71c609ab7a88a5b36d756160d0170672dd832050
Chris Hellmuth <chellmuth@gmail.com>
2018-11-02 13:54:13 -0700

    Rendering 1spp works

--- matlab_wrapper.py
+++ matlab_wrapper.py
@@ -1,5 +1,8 @@
-import subprocess
+from operator import mul
+
+import array
 import struct
+import subprocess
 
 def _write_randoms(randoms_n, out_path):
     out_file = open(out_path, "wb")
@@ -37,9 +40,43 @@ def f_n(randoms_memoryview):
     _write_randoms(randoms_n, "randoms.bin")
     return _f()
 
+# see order in runner.py or cjh.cpp
+X_INDEX = 3
+Y_INDEX = 4
+
+def render(cols, rows, spp, randoms_memoryview):
+    cols = int(cols)
+    rows = int(rows)
+    spp = int(spp)
+
+    total_samples_needed = cols * rows * spp
+
+    total_samples_provided, _ = randoms_memoryview.shape
+    if total_samples_needed != total_samples_provided:
+        raise BaseException(
+            "Need (%d x %d x %d) = %d samples. Received %d."
+            % (cols, rows, spp, total_samples_needed, total_samples_provided)
+        )
+
+    randoms_n = randoms_memoryview.tolist()
+
+    x_delta = 1. / cols
+    y_delta = 1. / rows
+    for row in range(rows):
+        for col in range(cols):
+            for sample in range(spp):
+                sample_index = cols * row + col + sample
+                x_sample = randoms_n[sample_index][X_INDEX]
+                y_sample = randoms_n[sample_index][Y_INDEX]
+                randoms_n[sample_index][X_INDEX] = (float(col) / cols) + x_sample * x_delta
+                randoms_n[sample_index][Y_INDEX] = (float(row) / rows) + y_sample * x_delta
+
+    _write_randoms(randoms_n, "randoms.bin")
+    return array.array("f", _f())
+
 def _f():
     return_code = subprocess.call(
-        ["mitsuba", "staircase/cjh-scene.xml"],
+        ["dist\\mitsuba", "cjh.xml"],
         env={"PATH": "./dist", "LD_LIBRARY_PATH": "./dist"}
     )
     if return_code != 0:
