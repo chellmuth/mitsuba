597eb458b81d9509470b36246e221b85d10e4ed1
Chris Hellmuth <chellmuth@gmail.com>
2018-12-13 18:06:09 -0800

    Re-enable render function

--- matlab_wrapper.py
+++ matlab_wrapper.py
@@ -45,8 +45,7 @@ def render(rows, cols, spp, randoms_memoryview):
                 randoms_n[sample_index][X_INDEX] = (float(col) / cols) + x_sample * x_delta
                 randoms_n[sample_index][Y_INDEX] = (float(row) / rows) + y_sample * y_delta
 
-    _write_randoms(randoms_n, "randoms.bin")
-    fs = _f()
+    luminances = _f(randoms_n)
 
     results = [ 0 for _ in range(rows * cols)]
     for row in range(rows):
@@ -54,7 +53,7 @@ def render(rows, cols, spp, randoms_memoryview):
             pixel_sum = 0
             for sample in range(spp):
                 result_index = ((cols * spp) * row) + (spp * col) + sample
-                pixel_sum += fs[result_index]
+                pixel_sum += luminances[result_index]
 
             pixel_value = pixel_sum / spp
             pixel_value = min(1, pixel_value ** (1/2.2))
